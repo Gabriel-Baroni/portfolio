bd1534f1c88dae3f8a8140340812fdc448c9a666
Gabriel-Baroni <gdepaulabaroni@gmail.com>
2026-02-21 20:20:57 -0300

    FEAT: Adiciona a estruturta base do projeto e o componente da PillNav

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import PillNav from './components/layout/Pillnav.jsx';
+
+
+const navItems = [
+    { label: 'Me', href: '#hero' },
+    { label: 'About', href: '#about' },
+    { label: 'Projects', href: '#projects' },
+    {label: 'Skills', href: '#skills'},
+    { label: 'Contact', href: '#contact' },
+  ];
+
+function App() {
+  return (
+    <div className="app-container">
+      <PillNav 
+        items={navItems} 
+        logo= {null}
+        baseColor="var(--bg-site)"           
+        pillColor="var(--bg-site)"           
+        pillTextColor="var(--nav-main)"     
+        hoveredPillTextColor="var(--bg-site)" 
+      />
+      <main>
+      </main>
+
+    </div>
+  )
+}
+
+export default App
